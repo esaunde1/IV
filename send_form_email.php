0b4cc6e159058bcae7e826e215b3e4a6100fcda9
eric <esaunde1@binghamton.edu>
2014-09-01 16:55:22 -0400

    sup dennis

--- send_form_email.php
+++ send_form_email.php
@@ -9,6 +9,11 @@
        $to = 'eric@insidervisit.com';
 
        $body = <<<HTML
+<p><strong>Message</strong></p>
+$email<br>
+<p><strong>Name</strong></p>
+$name<br>
+<p><strong>Email</strong></p>
 $message
 HTML;
 
@@ -16,9 +21,22 @@ HTML;
        $headers .= "Content-type: text/html\r\n";
 
        // send the email
-       mail($to, $subject, $body, $headers);
+       $mail_status = mail($to, $subject, $body, $headers);
 
-       // redirect afterwords, if needed
-       header('Location: index.html');
+
+    if ($mail_status) { ?>
+        <script language="javascript" type="text/javascript">
+            alert('Your email has been sent. We will get back to you shortly!');
+            window.location = 'index.html';
+        </script>
+    <?php
+    }
+    else { ?>
+        <script language="javascript" type="text/javascript">
+            alert('Something went wrong! Please try again.');
+            window.location = 'index.html';
+        </script>
+    <?php
+    }
 
 ?>
